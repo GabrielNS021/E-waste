2c148c97fced5496c3f072292f095c88f24cc817
Gabriel <112496072+GabrielNS021@users.noreply.github.com>
2025-11-07 23:19:05 +0000

    07-11

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/E-waste/", // caminho do repositório no GitHub Pages
   server: {
     host: "::",
     port: 8080,
